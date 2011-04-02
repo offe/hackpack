0ebcbab1af096778964f64a69cf832b713fafd9d
Mattias Svala <thebrasse@brasse.org>
2011-04-02 12:13:57 -0700

    Corrected project name in copyright notice.

--- hackpack.py
+++ hackpack.py
@@ -2,9 +2,9 @@
 
 #  Copyright (C) 2011  Oscar Lindberg, Mattias Svala
 #
-#  This file is part of SimpleNotes.
+#  This file is part of Hackpack.
 #
-#  SimpleNotes is free software; you can redistribute it and/or
+#  Hackpack is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
 #  as published by the Free Software Foundation; either version 2
 #  of the License, or (at your option) any later version.
